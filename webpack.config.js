dfe64b174d4c11e344753043294c40fca49699f0
Jamie Furlong <jamiefurlong16@gmail.com>
2022-10-16 19:27:04 +0100

    Webpack Environment Setup
    Completed intial set up of the webpack environment.
    Environment supports:
    - Images
    - Fonts
    - Multiple HTML pages.
    - css
    - Live Server
    
    Calibre and SFMono fonts were added to the assets folder.
    
    In addition, Babel was installed to support accessibility on older web browsers.

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Jamie Furlong',
+        template: './src/home.html',
+        filename: 'index.html',
+        minify: {
+            removeComments: true,
+            collapseWhitespace: true
+          }
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/about.html',
+        filename: 'about.html',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true
+        }
+      }),
+  ],
+  output: {
+    filename: '[name][contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devServer: {
+    static: {
+        directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+            }
+        }
+      }
+    ],
+  },
+};
