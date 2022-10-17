c2c24297a211e68f2b106ef36289d5524b60e185
Jamie Furlong <jamiefurlong16@gmail.com>
2022-10-17 14:23:51 +0100

    Second commit
    First draft of homepage design completed.
    HTML & CSS functional and page is mostly responsive.
    
    Animations and JS will be refined at a later date.
    CSS to be analysed and reduced where possible.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css'
+import sun from './sun.svg'
+import logo from './WWWLogo.svg'
+import me from './me.jpg'
+
+console.log(sun);
+const sunIm = new Image();
+const logoIm = new Image();
+const meIm = new Image();
+
+sunIm.src = sun;
+console.log(sunIm);
+const navLinks = document.querySelector('.nav-links');
+navLinks.appendChild(sunIm);
+sunIm.classList.add('pointer')
+
+logoIm.src = logo;
+const logoDiv = document.querySelector('.logo');
+logoDiv.appendChild(logoIm);
+
+meIm.src = me;
+const pictureSection = document.querySelector('.image');
+pictureSection.appendChild(meIm);
