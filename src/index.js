edf4370a040da32b0e768e1dd93c40cbbeca6f46
Jamie Furlong <jamiefurlong16@gmail.com>
2022-10-19 22:24:25 +0100

    About Section & Project Section Demo Setup
    Dummy content for the about and project sections were created.
    
    CSS not final and to be revised and made responsive in future commit.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import './style.css'
 import './mobile.css'
 import sun from './sun.svg'
 import logo from './WWWLogo.svg'
+import jamie from './jamie.jpg'
 import me from './me.jpg'
+import mario from './mario.png'
 
 console.log(sun);
 const sunIm = new Image();
 const logoIm = new Image();
 const meIm = new Image();
+const jamieIm = new Image();
+const marioIm = new Image();
 
 sunIm.src = sun;
 console.log(sunIm);
@@ -22,3 +26,12 @@ logoDiv.appendChild(logoIm);
 meIm.src = me;
 const pictureSection = document.querySelector('.image');
 pictureSection.appendChild(meIm);
+
+jamieIm.src = jamie;
+const aboutWrapper = document.querySelector('.about-wrapper');
+aboutWrapper.appendChild(jamieIm);
+
+marioIm.src = mario;
+const projectContainer = document.querySelector('.project-container');
+projectContainer.style.background = `linear-gradient(0deg, rgba(0, 0, 0, .7),  rgba(0, 0, 0, .7)), url(${mario}) no-repeat center center/cover`;
+
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
   output: {
     filename: '[name][contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
     clean: true,
     assetModuleFilename: '[name][ext]',
   },
